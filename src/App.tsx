e8f9edaaf86aa41840e20a52edaf200482d37932
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2025-03-28 14:01:40 -0300

    creating interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import { useState } from 'react'
 import './App.css'
 
+import logoImg from './assets/logo.png'
+
 function App() {
 
   return (
-    <div>
+    <div className="container">
+      <img
+        className="logo"
+        alt="Logo frases"
+        src={logoImg}
+      />
+
+      <h2 className="title">Categorias</h2>
+      <section className="category-area">
+        <button className="category-button">Motivação</button>
+        <button className="category-button">Bem estar</button>
+      </section>
+
+      <button className="button-frase">Gerar frase</button>
+
+      <p className="texto-frase">Alguma frase aqui</p>
+
     </div>
   )
 }
